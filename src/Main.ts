dceef82a593d22a91baacfd8f6ef69a6b42b0f98
Dennis Meelker <dennis.meelker@infi.nl>
2023-04-23 12:38:02 +0200

    Work

--- src/Main.ts
+++ src/Main.ts
@@ -1,15 +1,66 @@
-import { hello } from "./test";
-import { FrameCounter } from "./utilities/FrameCounter";
+import { FrameTime } from "./utilities/FrameTime";
+import { Size } from "./utilities/Trig";
+import { Viewport } from "./utilities/Viewport";
 
-function component() {
-    const element = document.createElement('div');
-    element.innerHTML = hello("piet");
+export abstract class Screen {
+    protected _viewport: Viewport;
 
-    return element;
+    public constructor(viewport: Viewport) {
+        this._viewport = viewport;
+    }
+
+    public abstract update(time: FrameTime): void;
+    public abstract render(): void;
+
+    protected get viewport(): Viewport { return this._viewport; }
 }
 
-let counter = new FrameCounter();
-counter.frame();
-console.log(counter.fps);
+class TestScreen extends Screen {
+    public update(time: FrameTime): void {
 
-document.body.appendChild(component());
+    }
+
+    public render(): void {
+        this.viewport.clearCanvas();
+    }
+
+}
+
+export class ScreenManager {
+    private _activeScreen: Screen;
+
+    public constructor(activeScreen: Screen) {
+        this._activeScreen = activeScreen;
+        this.activateScreen(activeScreen);
+    }
+
+    public changeScreen(newScreen: Screen) {
+        this.deactivateScreen(this._activeScreen);
+        this._activeScreen = newScreen;
+        this.activateScreen(newScreen);
+    }
+
+    private activateScreen(screen: Screen) {
+
+    }
+
+    private deactivateScreen(screen: Screen) {
+
+    }
+
+    public update(time: FrameTime) {
+        this._activeScreen.update(time);
+    }
+
+    public render() {
+        this._activeScreen.render();
+    }
+
+    public get activeScreen(): Screen { return this._activeScreen; }
+}
+
+let container = document.getElementById("game");
+let viewport = new Viewport(new Size(800, 600), container);
+
+let testScreen = new TestScreen(viewport);
+let screenManager = new ScreenManager(testScreen);
---
+++ src/utilities/Viewport.ts
@@ -0,0 +1,49 @@
+import { Size } from "./Trig";
+import * as Dom from "./Dom";
+
+export class Viewport {
+    private readonly _size: Size;
+    private readonly _parentElement;
+    private _element: HTMLElement;
+    private readonly _canvas: HTMLCanvasElement;
+    private readonly _canvasContext: CanvasRenderingContext2D;
+    private readonly _uiElement: HTMLElement;
+
+    public constructor(size: Size, element: HTMLElement) {
+        this._size = size;
+        this._parentElement = element;
+
+        this._element = document.createElement("div");
+        this._element.style.position = "relative";
+        Dom.setSize(this._element, this._size);
+
+        this._canvas = document.createElement("canvas");
+        this._canvas.width = size.width;
+        this._canvas.height = size.height;
+        this._canvas.style.position = "absolute";
+        this._canvas.style.inset = "0";
+        this._element.appendChild(this._canvas);
+
+        this._canvasContext = this._canvas.getContext("2d");
+
+        this._uiElement = document.createElement("div");
+        this._uiElement.style.position = "absolute";
+        this._uiElement.style.inset = "0";
+        this._element.appendChild(this._uiElement);
+
+        this._parentElement.appendChild(this._element);
+    }
+
+    public clearCanvas() {
+        this._canvasContext.beginPath();
+        this._canvasContext.fillStyle = "black";
+        this._canvasContext.fillRect(0, 0, this.size.width, this.size.height);
+    }
+
+    public get size() { return this._size; }
+    public get width() { return this._size.width; }
+    public get height() { return this._size.height; }
+
+    public get context() { return this._canvasContext; }
+    public get uiElement() { return this._uiElement; }
+}
